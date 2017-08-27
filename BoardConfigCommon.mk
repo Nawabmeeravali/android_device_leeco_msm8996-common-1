befe8ccc840c04c19837e49d27556a87b1206438
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-27 14:16:53 +0200

    msm8996: don't include qcom sepolicies
    Change-Id: Ie4831bd462539d4474e1f95bef26b75e9a93d452

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -263,6 +263,6 @@ endif
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 -include vendor/leeco/msm8996-common/BoardConfigVendor.mk
