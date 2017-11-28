6a2bdc532d17dda5ec55b92be2e3e77810790486
1nfrag <sil3ntcll@gmail.com>
2017-11-29 07:56:08 +0800

    Remove camera device and rely solely on wrapper
    Change-Id: I6637c12572689b67398598a6fcadec2c4d9a1b3a

--- msm8996.mk
+++ msm8996.mk
@@ -154,7 +154,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
-    camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     libshim_camera \
     camera.msm8996 \
