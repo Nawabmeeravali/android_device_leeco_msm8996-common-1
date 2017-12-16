3958d8d37b184c69538c31d93e646a2ca473dc2f
1nfrag <sil3ntcll@gmail.com>
2017-12-16 10:10:32 +0800

    PowerHAL 1.1
    Change-Id: I05bbf96726699c98d4b045ccee32207551529e92

--- msm8996.mk
+++ msm8996.mk
@@ -303,9 +303,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service-qti \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8996
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
