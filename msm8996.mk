6299d0679ae6446707efa5fb13b3bb651ae838db
1nfrag <sil3ntcll@gmail.com>
2017-12-28 13:09:29 +0800

    Revert "Add prebuilt libc"
    This reverts commit b647387c5b5fd288cb6cc41ccecd5757877e3a41.
    
    Change-Id: I42534ea286a7b64f2e03c1e02d859223c52cb43f

--- msm8996.mk
+++ msm8996.mk
@@ -160,10 +160,6 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     Snap
 
-# Copy prebuilt libc
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/libshims/libc.so:system/lib/libc.so
-
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
