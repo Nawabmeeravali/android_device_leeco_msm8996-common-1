ae28521de3100369aab5dc9983b8b8d4d966262d
celtare21 <celtare21@gmail.com>
2018-01-24 01:48:26 +0000

    Try to fix FP

--- msm8996.mk
+++ msm8996.mk
@@ -211,9 +211,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom \
-    fingerprintd \
-    fingerprint.msm8996
+    android.hardware.biometrics.fingerprint@2.0-service-custom
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
